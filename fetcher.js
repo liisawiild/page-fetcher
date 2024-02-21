f935533b1f39366dc175dd94def66bd066f6b1b6
Lisa Wild <lisakwild@gmail.com>
2024-02-21 10:27:59 -0800

    comment out prints not asked for

--- fetcher.js
+++ fetcher.js
@@ -1,15 +1,33 @@
-const cmdLine = process.argv.slice(2);   //slice turns it into array
-// cmdLine[2] = URL, --- download resource at this location
-//cmdLine[3] = local path  --- save resource to this location
-//print message to server "Downloaded and saved # of bytes to local path"
 
 const request = require('request');
+const fs = require('fs');
 
-request(cmdLine[2], (error, response, body) => {
-  console.log(`Downloaded and saved ${bytes} bytes to ${cmdLine[3]}`)
-  console.log('---error:', error);
-  console.log('---response:', response);
-  console.log('---body:', body);
 
-}))
+const cmdLine = process.argv.slice(2);
+let URL = cmdLine[0];
+let path = cmdLine[1];
+
+request(URL, (error, response, body) => {
+  // console.log('---error:', error);
+  // console.log('---response:', response);
+  // console.log('---body:', body);
+
+  //function to write/download (fs = filesystem) body to a local file (NOT writeFileSync, or pipe function)
+  fs.writeFile('./fetchedData.html', body, err => {
+    if (err) {
+      console.log(err);
+    } else {
+      let bytes = body.length;
+      console.log(`Downloaded and saved ${bytes} bytes to ${path}`);
+    }
+  });
+
+});
+
+
+
+
+
+
+
 
