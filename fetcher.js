8b2f19b5dcb48d648269056afc0b5dfffbe49030
Lisa Wild <lisakwild@gmail.com>
2024-02-21 10:29:36 -0800

    remove commented-out code and refactor

--- fetcher.js
+++ fetcher.js
@@ -2,17 +2,11 @@
 const request = require('request');
 const fs = require('fs');
 
-
 const cmdLine = process.argv.slice(2);
 let URL = cmdLine[0];
 let path = cmdLine[1];
 
 request(URL, (error, response, body) => {
-  // console.log('---error:', error);
-  // console.log('---response:', response);
-  // console.log('---body:', body);
-
-  //function to write/download (fs = filesystem) body to a local file (NOT writeFileSync, or pipe function)
   fs.writeFile('./fetchedData.html', body, err => {
     if (err) {
       console.log(err);
